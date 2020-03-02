04fed74be26b57f6c3a059caf8272e4e0eb014f6
user <user@asus.obsd>
2020-03-02 17:09:13 +1000

    no-address

--- conf.py
+++ conf.py
@@ -1,10 +1,10 @@
 from libz.peewee import SqliteDatabase
 
 DATA='./data'
-URL='gk11.ru'
+URL='none.gk11.ru'
 BIND='127.0.0.1'
 SHOWMSGFROM='never' # always, newmsg, never
-STREET='gk11.ru'
+STREET='none'
 
 db = SqliteDatabase('%s/bb.db' % DATA)
 userdb = SqliteDatabase('%s/user.db' % DATA)
